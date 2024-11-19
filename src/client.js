d46299bef2d469bb0246c793073056fa4b2529ee
Frankelly Guzman <FrankellyRGuzman@gmail.com>
2024-11-18 19:28:54 -0500

    Testing fix

--- src/client.js
+++ src/client.js
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_API;
 
+console.log("URL: ", supabaseUrl);
+console.log("KEY: ", supabaseKey);
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
